4454c43a599259b21968b034dc6edb638f6e8cab
aditisingh9725aps-web <aditisingh9725aps@gmail.com>
2026-07-14 13:06:31 +0530

    Add files via upload
    output:
    Brand: Toyota
    Model: Fortuner
    Toyota Fortuner is starting

---
+++ practiceoop.py
@@ -0,0 +1,32 @@
+#vechile managment system using oops 
+# Parent Class
+class Vehicle:
+    def __init__(self, brand):
+        self.brand = brand
+
+    def start(self):
+        print("Vehicle is starting")
+
+
+# Child Class (Inheritance)
+class Car(Vehicle):
+    def __init__(self, brand, model):
+        super().__init__(brand)
+        self.__model = model      # Encapsulation
+
+    # Polymorphism (Method Overriding)
+    def start(self):
+        print(self.brand, self.__model, "is starting")
+
+    def display(self):
+        print("Brand:", self.brand)
+        print("Model:", self.__model)
+
+
+# Object Creation
+car = Car("Toyota", "Fortuner")
+
+car.display()
+car.start()
+
+
